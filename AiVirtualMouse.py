1fe55cbe5671477bae1fd16acaba1ceff5bd74f3
TOM SAJU <tomsaju33@gmail.com>
2022-09-18 19:49:55 +0530

    Fixed mediapipe and autopy version. changed python version to 3.7.0 64-bit

--- AiVirtualMouse.py
+++ AiVirtualMouse.py
@@ -14,11 +14,31 @@ heightCamera = 480
 capture = cv2.VideoCapture(0)  # 0: External WebCam and 1 Other
 capture.set(3, widthCamera)
 capture.set(4, heightCamera)
+pTime = 0
 
+
+# Detect hand marks
+detector = htm.handDetector(maxHands=1) # Expecting only 1 hand
 
 while True:
-
+    # 1. Find hand Landmarks
     success, img = capture.read()
+    img = detector.findHands(img)
 
+    # Finding positions of the hands.
+    lmList, bbox = detector.findPosition(img)
+
+
+    # 2. Get the tip of the index and middle finger
+    # 3. Check which fingers are up
+    # 4. Only Index finger up means it is in moving mode
+
+    # Frame Rate
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+    # Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
